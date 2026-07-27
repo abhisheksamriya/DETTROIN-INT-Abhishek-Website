9421d6ca443a347b8c173c02aa3f1dd81fc4e0b5
abhisheksamriya <abhisheksamriya2004@gmail.com>
2026-07-27 18:35:39 +0530

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Best School in Aligarh | Top Schools in Aligarh – Excellence International School",
+  description: "Excellence International School is among the best schools in Aligarh, Uttar Pradesh, offering quality education and holistic development.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col"  cz-shortcut-listen="true">{children}</body>
+    </html>
+  );
+}
